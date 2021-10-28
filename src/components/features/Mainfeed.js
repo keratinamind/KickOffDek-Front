da4c05fba016020152631d2cb94b6b7972a550ee
OhmNattanon <nattte35@gmail.com>
2021-10-28 15:53:28 +0700

    filter feed done

--- src/components/features/Mainfeed.js
+++ src/components/features/Mainfeed.js
@@ -26,114 +26,136 @@ function Mainfeed() {
     setCategory(arr);
   };
 
-  const createFilterFeed = () => {
-    if (category.length === 0) {
-      return currentFeed;
-    } else {
-      const filteredFeed = [];
-      for (let x of category) {
-        const newItem = currentFeed.filter((i) => i.Category.name === x);
-        filteredFeed.push(newItem);
-      }
-      console.log(filteredFeed);
-      return filteredFeed;
-    }
-  };
-
-  const filteredFeed = createFilterFeed();
-
+  const filteredFeed = currentFeed.filter((i) => {
+    const isTrue = category.includes(i.Category?.name);
+    return isTrue;
+  });
+  console.log(category);
   console.log(filteredFeed);
+
+  const isClicked = (i) =>
+    category.includes(i) ? "bg-purple-600 text-white" : "";
 
   return (
     <>
       <div className="grid grid-cols-12 py-10 w-10/12 mx-auto">
         <div className="col-span-10">
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "art"
+            )}`}
             onClick={() => handleClickCategory("art")}
           >
             Art &amp; Craft
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "community"
+            )}`}
             onClick={() => handleClickCategory("community")}
           >
             Community &amp; Social goods
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "design"
+            )}`}
             onClick={() => handleClickCategory("design")}
           >
             Design &amp; Fashion
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "environment"
+            )}`}
             onClick={() => handleClickCategory("environment")}
           >
             Environmental
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "event"
+            )}`}
             onClick={() => handleClickCategory("event")}
           >
             Events
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "film"
+            )}`}
             onClick={() => handleClickCategory("film")}
           >
             Film &amp; Video
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "food"
+            )}`}
             onClick={() => handleClickCategory("food")}
           >
             Food &amp; Drinks
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "music"
+            )}`}
             onClick={() => handleClickCategory("music")}
           >
             Music
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "performance"
+            )}`}
             onClick={() => handleClickCategory("performance")}
           >
             Performance
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "photography"
+            )}`}
             onClick={() => handleClickCategory("photography")}
           >
             Photography
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "publishing"
+            )}`}
             onClick={() => handleClickCategory("publishing")}
           >
             Publishing
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "research"
+            )}`}
             onClick={() => handleClickCategory("research")}
           >
             Research
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "socialEnterprise"
+            )}`}
             onClick={() => handleClickCategory("socialEnterprise")}
           >
             Social Enterprise
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "sport"
+            )}`}
             onClick={() => handleClickCategory("sport")}
           >
             Sport
           </button>
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              "tech"
+            )}`}
             onClick={() => handleClickCategory("tech")}
           >
             Tech &amp; Games
@@ -141,7 +163,9 @@ function Mainfeed() {
         </div>
         <div className="col-span-2">
           <button
-            className="m-2 p-1.5 rounded-md border border-black hover:text-white hover:bg-gray-900"
+            className={`m-2 p-1.5 rounded-md border border-black hover:bg-purple-600 hover:text-white active:bg-purple-600 ${isClicked(
+              []
+            )}`}
             onClick={() => setCategory([])}
           >
             Trending
@@ -153,11 +177,17 @@ function Mainfeed() {
           <h1 className="font-extrabold text-lg">Trending Projects</h1>
         </div>
         <div className="grid grid-cols-3">
-          {filteredFeed.map((item, idx) => (
-            <div className="m-2 pb-4" key={idx}>
-              <Cardproject {...item} />
-            </div>
-          ))}
+          {category.length > 0
+            ? filteredFeed.map((item, idx) => (
+                <div className="m-2 pb-4" key={idx}>
+                  <Cardproject {...item} />
+                </div>
+              ))
+            : currentFeed.map((item, idx) => (
+                <div className="m-2 pb-4" key={idx}>
+                  <Cardproject {...item} />
+                </div>
+              ))}
         </div>
         <div className="flex justify-center mb-10">
           <button className="border border-black bg-purple-600 rounded-md p-3 text-white">
